3577d8e734c7159d112ef1ad5dc83b112cbc17c4
Louis <louispaulse07@gmail.com>
2019-12-18 13:42:58 +0200

    Change requests passed to allow error messages to be displayed

--- api.py
+++ api.py
@@ -11,8 +11,6 @@ class OMBdApi:
         self.api_url = f'{self.api_url}&t={movie_title}'
 
         response = requests.post(self.api_url)
-        if response.status_code == 200:
-            response = json.loads(response.content.decode('utf-8'))
-            print(response)
-        else:
-            return None
+
+        response = json.loads(response.content.decode('utf-8'))
+        return response
